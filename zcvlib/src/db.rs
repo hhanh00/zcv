22da8ff4c9fd28e73afa976f42dcc020d0d82c29
Hanh Huynh Huu <hanh425@gmail.com>
2025-12-15 07:31:48 +0800

    feat: scan for notes

--- zcvlib/src/db.rs
+++ zcvlib/src/db.rs
@@ -1,5 +1,12 @@
 use bip39::Mnemonic;
-use sqlx::{SqliteConnection, query};
+use orchard::{
+    Address, Note,
+    keys::{FullViewingKey, IncomingViewingKey, SpendingKey},
+};
+use pasta_curves::Fp;
+use sqlx::{SqliteConnection, query, query_as};
+use zcash_protocol::consensus::{Network, NetworkConstants};
+use zip32::{AccountId, Scope};
 
 use crate::{ZCVResult, error::IntoAnyhow};
 
@@ -44,14 +51,14 @@ pub async fn create_schema(conn: &mut SqliteConnection) -> ZCVResult<()> {
         id_note INTEGER PRIMARY KEY,
         question INTEGER NOT NULL,
         height INTEGER NOT NULL,
-        pool INTEGER NOT NULL,
+        scope INTEGER NOT NULL,
         position INTEGER NOT NULL,
         nf BLOB NOT NULL,
         rho BLOB NOT NULL,
         diversifier BLOB NOT NULL,
         rseed BLOB NOT NULL,
         value INTEGER NOT NULL,
-        UNIQUE (question, pool, position))",
+        UNIQUE (question, position))",
     )
     .execute(&mut *conn)
     .await?;
@@ -66,15 +73,85 @@ pub async fn create_schema(conn: &mut SqliteConnection) -> ZCVResult<()> {
     Ok(())
 }
 
-pub async fn set_account_seed(conn: &mut SqliteConnection, mnemonic: &str, aindex: u32) -> ZCVResult<()> {
+pub async fn set_account_seed(
+    conn: &mut SqliteConnection,
+    mnemonic: &str,
+    aindex: u32,
+) -> ZCVResult<()> {
     Mnemonic::parse(mnemonic).anyhow()?;
-    query("INSERT INTO account(id_account, seed, aindex)
+    query(
+        "INSERT INTO account(id_account, seed, aindex)
     VALUES (0, ?1, ?2) ON CONFLICT DO UPDATE
-    SET seed = excluded.seed, aindex = excluded.aindex")
+    SET seed = excluded.seed, aindex = excluded.aindex",
+    )
     .bind(mnemonic)
     .bind(aindex)
     .execute(conn)
     .await?;
+    Ok(())
+}
+
+pub async fn get_ivks(
+    network: &Network,
+    conn: &mut SqliteConnection,
+) -> ZCVResult<(IncomingViewingKey, IncomingViewingKey)> {
+    let (seed, aindex): (String, u32) =
+        query_as("SELECT seed, aindex FROM account WHERE id_account = 0")
+            .fetch_one(conn)
+            .await?;
+    let spk = derive_spending_key(network, seed, aindex)?;
+    let fvk = FullViewingKey::from(&spk);
+    let ivks = (fvk.to_ivk(Scope::External), fvk.to_ivk(Scope::Internal));
+    Ok(ivks)
+}
+
+fn derive_spending_key(network: &Network, seed: String, aindex: u32) -> ZCVResult<SpendingKey> {
+    let mnemonic = Mnemonic::parse(&seed).anyhow()?;
+    let seed = mnemonic.to_seed("");
+    let spk = SpendingKey::from_zip32_seed(
+        &seed,
+        network.coin_type(),
+        AccountId::const_from_u32(aindex),
+    )
+    .anyhow()?;
+    Ok(spk)
+}
+
+pub async fn store_received_note(
+    conn: &mut SqliteConnection,
+    election_domain: Fp,
+    fvk: &FullViewingKey,
+    note: &Note,
+    address: &Address,
+    pre_snapshot: bool,
+    height: u32,
+    position: u32,
+    question: u32,
+    scope: u32,
+) -> ZCVResult<()> {
+    let nf = if pre_snapshot {
+        note.nullifier(fvk)
+    } else {
+        note.nullifier_domain(fvk, election_domain)
+    };
+
+    query(
+        "INSERT INTO notes
+    (question, height, scope, position, nf, rho, diversifier, rseed, value)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    )
+    .bind(question)
+    .bind(height)
+    .bind(scope)
+    .bind(position)
+    .bind(nf.to_bytes().as_slice())
+    .bind(note.rho().to_bytes().as_slice())
+    .bind(address.diversifier().as_array().as_slice())
+    .bind(note.rseed().as_bytes().as_slice())
+    .bind(note.value().inner() as i64)
+    .execute(conn)
+    .await?;
+
     Ok(())
 }
 
@@ -98,9 +175,10 @@ mod tests {
 
         let (c,): (u32,) = sqlx::query_as(
             "SELECT 1 FROM sqlite_master WHERE type = 'table'
-            AND name = 'elections'")
-            .fetch_one(&mut *conn)
-            .await?;
+            AND name = 'elections'",
+        )
+        .fetch_one(&mut *conn)
+        .await?;
 
         assert_eq!(c, 1);
 
