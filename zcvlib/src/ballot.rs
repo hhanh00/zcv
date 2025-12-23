af201f98288b36f81b33ffa8b42f551bf27cd4f6
Hanh Huynh Huu <hanh425@gmail.com>
2025-12-23 17:37:13 +0800

    chore: add tendermint dependencies

--- zcvlib/src/ballot.rs
+++ zcvlib/src/ballot.rs
@@ -2,11 +2,11 @@ use ff::PrimeField;
 use orchard::{
     Address,
     keys::{FullViewingKey, PreparedIncomingViewingKey, Scope},
-    vote::{Ballot, BallotAnchors, BallotData, encrypt_ballot_action, try_decrypt_ballot},
+    vote::{BallotAnchors, BallotData, encrypt_ballot_action, try_decrypt_ballot},
 };
 use pasta_curves::Fp;
 use rand_core::{CryptoRng, RngCore};
-use sqlx::{SqliteConnection, query};
+use sqlx::SqliteConnection;
 use zcash_protocol::consensus::Network;
 
 use crate::{
