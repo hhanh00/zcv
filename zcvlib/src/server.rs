a99daaa4944978f8dbbf4b5479295f1d945b99de
hhanh00 <hanh425@gmail.com>
2026-01-05 22:30:12 +0800

    feat: apphash calculation (#44)

--- zcvlib/src/server.rs
+++ zcvlib/src/server.rs
@@ -1,10 +1,19 @@
+use crate::{
+    ZCVError, ZCVResult,
+    context::Context,
+    db::{get_apphash, get_election, store_apphash},
+    error::IntoAnyhow,
+    server::rpc::submit_ballot,
+    vote_rpc::{Ballot, VoteMessage},
+};
 use base64::{Engine, prelude::BASE64_STANDARD};
+use blake2b_simd::Params;
 use parking_lot::Mutex;
 use prost::Message;
 use rocket::{figment::Figment, routes};
 use rocket_cors::CorsOptions;
 use serde_json::Value;
-use sqlx::{SqliteConnection, query_as};
+use sqlx::{Acquire, SqlitePool};
 use std::{
     net::{Ipv4Addr, SocketAddrV4},
     sync::Arc,
@@ -14,9 +23,6 @@ use tendermint_abci::{Application, ServerBuilder};
 use tendermint_proto::abci::{
     ExecTxResult, RequestCheckTx, RequestFinalizeBlock, RequestInfo, RequestPrepareProposal,
     ResponseCheckTx, ResponseFinalizeBlock, ResponseInfo, ResponsePrepareProposal,
-};
-use crate::{
-    ZCVError, ZCVResult, context::Context, db::get_election, error::IntoAnyhow, server::rpc::submit_ballot, vote_rpc::{Ballot, VoteMessage}
 };
 
 pub mod rpc;
@@ -29,8 +35,8 @@ pub struct Server {
 }
 
 impl Server {
-    pub async fn new(conn: SqliteConnection, id_election: u32) -> ZCVResult<Self> {
-        let server = ServerState::new(conn, id_election).await?;
+    pub async fn new(pool: SqlitePool, id_election: u32) -> ZCVResult<Self> {
+        let server = ServerState::new(pool, id_election).await?;
         Ok(Self {
             state: Arc::new(Mutex::new(server)),
         })
@@ -38,19 +44,21 @@ impl Server {
 }
 
 pub struct ServerState {
-    pub conn: SqliteConnection,
-    pub height: u32,
+    pub pool: SqlitePool,
+    pub id_election: u32,
+    pub start_height: u32,
 }
 
 impl ServerState {
-    pub async fn new(mut conn: SqliteConnection, id_election: u32) -> ZCVResult<Self> {
+    pub async fn new(pool: SqlitePool, id_election: u32) -> ZCVResult<Self> {
+        let mut conn = pool.acquire().await?;
         let e = get_election(&mut conn, id_election).await?;
-        let (h,): (Option<u32>,) = query_as("SELECT MAX(height) FROM ballots")
-            .fetch_one(&mut conn)
-            .await?;
-        let height = h.unwrap_or(e.end) + 1;
 
-        Ok(Self { conn, height })
+        Ok(Self {
+            pool,
+            id_election,
+            start_height: e.end,
+        })
     }
 
     pub fn check_ballot(&mut self, ballot: Ballot) -> ZCVResult<()> {
@@ -131,18 +139,61 @@ impl Application for Server {
 
     // Process the block that was voted on by the validators
     fn finalize_block(&self, request: RequestFinalizeBlock) -> ResponseFinalizeBlock {
-        // let rt = tokio::runtime::Runtime::new().unwrap();
-        // Store ballot in db, etc.
-        // rt.block_on(store_ballot(&mut self.conn, self.height, b))?;
-        let tx_results: Vec<_> = request
-            .txs
-            .into_iter()
-            .map(|tx| ExecTxResult {
-                code: 0,
-                data: tx,
-                ..ExecTxResult::default()
+        let RequestFinalizeBlock {
+            txs, hash, height, ..
+        } = request;
+        tracing::info!("Hash {} height {height}", hex::encode(&hash));
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let mut state = self.state.lock();
+        let tx_results = rt
+            .block_on(async move {
+                let id_election = state.id_election;
+                let mut conn = state.pool.acquire().await?;
+                let mut db_tx = conn.begin().await?;
+                let apphash = get_apphash(&mut db_tx, id_election).await?;
+                let mut hasher = Params::new()
+                    .personal(b"ZCVote___AppHash")
+                    .hash_length(32)
+                    .key(&apphash)
+                    .to_state();
+                let mut tx_results = vec![];
+                for mut tx in txs {
+                    let tx_copy = tx.clone();
+                    hasher.update(&tx_copy);
+                    let mut finalize = || {
+                        let msg = VoteMessage::decode(&mut tx)?;
+                        if let Some(m) = msg.type_oneof
+                            && let crate::vote_rpc::vote_message::TypeOneof::Ballot(ballot) = m
+                        {
+                            state.check_ballot(ballot)?;
+                        }
+                        Ok::<_, ZCVError>(())
+                    };
+                    let result = match finalize() {
+                        Ok(_) => ExecTxResult {
+                            code: 0,
+                            ..ExecTxResult::default()
+                        },
+                        Err(error) => ExecTxResult {
+                            code: 1,
+                            data: tx_copy,
+                            log: error.to_string(),
+                            info: "Error in finalization".to_string(),
+                            ..ExecTxResult::default()
+                        },
+                    };
+                    tx_results.push(result);
+                }
+                let apphash = hasher.finalize().as_bytes().to_vec();
+                store_apphash(&mut db_tx, id_election, &apphash).await?;
+
+                // TODO
+                // store_ballot(&mut self.conn, self.height, b).await?;
+                db_tx.commit().await?;
+                Ok::<_, ZCVError>(tx_results)
             })
-            .collect();
+            .expect("Fatal Failure in FinalizeBlock");
+
         ResponseFinalizeBlock {
             tx_results,
             ..ResponseFinalizeBlock::default()
@@ -172,8 +223,7 @@ pub async fn submit_tx(tx_bytes: &[u8], port: u16) -> ZCVResult<Value> {
 }
 
 pub async fn run_cometbft_app(context: &Context, id_election: u32, port: u16) -> ZCVResult<()> {
-    let conn = context.connect().await?.detach();
-    let app = Server::new(conn, id_election).await?;
+    let app = Server::new(context.pool.clone(), id_election).await?;
     let server = ServerBuilder::new(1_000_000)
         .bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, port), app)
         .anyhow()?;
