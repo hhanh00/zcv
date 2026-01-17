55b73c66f2ef44ccc85c97a3657f62cf9b2166a3
Hanh Huynh Huu <hanh425@gmail.com>
2026-01-17 18:28:55 +0800

    feat(setup): config read/parse

--- setup/src/main.rs
+++ setup/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use anyhow::Result;
+use tracing::info;
+use clap::Parser;
+use figment::{Figment, providers::{Format, Yaml}};
+use zcv_setup::config::{CLIArgs, Config};
+
+fn main() -> Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_ansi(false)
+        .compact()
+        .finish();
+    let _ = tracing::subscriber::set_global_default(subscriber);
+
+    let args = CLIArgs::parse();
+    let config_path = args.config_path.unwrap_or("zcv-setup.yml".to_string());
+    let config: Config = Figment::new().merge(Yaml::file(&config_path)).extract()?;
+    info!("Cluster Config {}", serde_json::to_string_pretty(&config)?);
+
+    Ok(())
+}
