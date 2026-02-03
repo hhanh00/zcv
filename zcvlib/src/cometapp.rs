ec8c01570e4bb58ce04406812fb87a32e256f16e
hhanh00 <hanh425@gmail.com>
2026-02-03 21:37:35 +0800

    feat: add command line parser to voter-cometbft (#94)

--- zcvlib/src/cometapp.rs
+++ zcvlib/src/cometapp.rs
@@ -1,30 +1,45 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use clap::Parser;
 use figment::{
     Figment,
-    providers::{Format, Toml},
+    providers::{Format, Serialized, Toml},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tokio::{sync::Mutex, task::LocalSet};
 use tonic::transport::Server;
 use zcvlib::{
-    ZCVError, context::BFTContext, db::create_schema, server::{rpc::ZCVServer, run_cometbft_app}, vote_rpc::vote_streamer_server::VoteStreamerServer
+    ZCVError,
+    context::BFTContext,
+    db::create_schema,
+    server::{rpc::ZCVServer, run_cometbft_app},
+    vote_rpc::vote_streamer_server::VoteStreamerServer,
 };
 
-#[derive(Deserialize)]
+#[derive(Parser, Serialize, Deserialize, Debug)]
 pub struct Config {
-    pub cometrpc_port: u16,
-    pub cometbft_port: u16,
-    pub grpc_port: u16,
-    pub db_path: String,
-    pub lwd_url: String,
-    pub hash: String,
+    #[clap(short = 'r', long, value_parser)]
+    pub cometrpc_port: Option<u16>,
+    #[clap(short = 'b', long, value_parser)]
+    pub cometbft_port: Option<u16>,
+    #[clap(short = 'g', long, value_parser)]
+    pub grpc_port: Option<u16>,
+    #[clap(short, long, value_parser)]
+    pub db_path: Option<String>,
+    #[clap(short, long, value_parser)]
+    pub lwd_url: Option<String>,
+    #[clap(short = 'e', long, value_parser)]
+    pub hash: Option<String>,
 }
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    let config: Config = Figment::new().merge(Toml::file("zcv.toml")).extract()?;
+    let config = Config::parse();
+    let config: Config = Figment::new()
+        .merge(Toml::file("zcv.toml"))
+        .join(Serialized::defaults(config))
+        .extract()?;
     let Config {
         cometrpc_port,
         cometbft_port,
@@ -33,12 +48,18 @@ pub async fn main() -> Result<()> {
         lwd_url,
         hash,
     } = config;
+    let cometrpc_port = cometrpc_port.unwrap_or(26657);
+    let cometbft_port = cometbft_port.unwrap_or(26658);
+    let grpc_port = grpc_port.unwrap_or(9010);
+    let db_path = db_path.unwrap_or("vote.db".to_string());
+    let lwd_url = lwd_url.unwrap_or("https://zec.rocks".to_string());
+    let hash = hash.expect("Election hash must be specified");
+
     let hash = hex::decode(&hash)?;
     let context = BFTContext::new(&db_path, &lwd_url, cometrpc_port).await?;
     {
         let mut conn = context.connect().await?;
         create_schema(&mut conn).await?;
-
     }
 
     let context = Arc::new(Mutex::new(context));
