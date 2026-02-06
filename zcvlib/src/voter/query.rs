f932858c50828ae851d61206fd3eac5ea43fe633
Hanh Huynh Huu <hanh425@gmail.com>
2026-02-06 16:42:31 +0800

    fix: get_balance

--- zcvlib/src/voter/query.rs
+++ zcvlib/src/voter/query.rs
@@ -23,7 +23,7 @@ impl Query {
     }
 
     async fn get_balance(hash: String, id_account: i32, idx_question: i32, context: &GQLContext) -> FieldResult<BigDecimal> {
-        let b = zcvlib::api::simple::get_balance(hash, idx_question as u32, id_account as u32, &context.0).await?;
+        let b = zcvlib::api::simple::get_balance(hash, id_account as u32, idx_question as u32, &context.0).await?;
         let digits = BigInt::from(b);
         let zec = BigDecimal::from_bigint(digits, 8);
         Ok(zec)
