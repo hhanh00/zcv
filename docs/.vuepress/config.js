c1d1d58f9753a072964ae4616605ebe5eede1b1b
hhanh00 <hanh425@gmail.com>
2026-02-19 14:20:00 +0800

    chore: documentation site (#124)

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
+import { markdownImagePlugin } from "@vuepress/plugin-markdown-image";
+import { markdownMathPlugin } from "@vuepress/plugin-markdown-math";
+import { markdownExtPlugin } from "@vuepress/plugin-markdown-ext";
+import { markdownChartPlugin } from '@vuepress/plugin-markdown-chart'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default defineUserConfig({
+  title: "ZCV Documentation",
+  base: '/zcv/',
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    sidebar: [
+      {
+        text: "How To",
+        prefix: "/how-to/",
+        children: [
+            "overview",
+            "format",
+            "creator",
+            "voter",
+            "authority",
+            "results",
+        ],
+      },
+    ],
+  }),
+  plugins: [
+    searchPlugin({}),
+    markdownImagePlugin({
+      figure: true,
+      lazyload: true,
+      size: true,
+    }),
+    markdownMathPlugin({}),
+    markdownExtPlugin({
+      gfm: true,
+      breaks: false,
+    }),
+    markdownChartPlugin({
+      mermaid: true,
+    }),
+  ],
+});
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: Zcash Coin Voting
+tagline: Coin Weighted Voting for Zcash
+actions:
+    - text: How To
+      link: /how-to/overview.md
+---
