be5299ddc10ea5327e89bbb6677454208131dbec
Hanh Huynh Huu <hanh425@gmail.com>
2026-02-25 22:54:55 +0800

    chore: remove flutter rust bridge

--- zcvlib/src/context.rs
+++ zcvlib/src/context.rs
@@ -1,7 +1,43 @@
 
-use sqlx::{Sqlite, pool::PoolConnection};
+use std::time::Duration;
 
-use crate::{ZCVResult, api::Context};
+use anyhow::Result;
+use sqlx::{Sqlite, SqlitePool, pool::PoolConnection, sqlite::SqliteConnectOptions};
+
+use crate::db::create_schema;
+
+use crate::ZCVResult;
+
+// #[frb(opaque)]
+#[derive(Clone)]
+pub struct Context {
+    // #[flutter_rust_bridge::frb(ignore)]
+    pub pool: SqlitePool,
+    pub lwd_url: String,
+    pub election_url: String,
+}
+
+impl Context {
+    pub async fn new(db_path: &str, lwd_url: &str, election_url: &str) -> Result<Context> {
+        let connect_options = SqliteConnectOptions::new()
+            .create_if_missing(true)
+            .busy_timeout(Duration::from_mins(1))
+            .filename(db_path);
+        let pool = SqlitePool::connect_with(connect_options).await?;
+        let mut conn = pool.acquire().await?;
+        create_schema(&mut conn).await?;
+        Ok(Context {
+            pool,
+            lwd_url: lwd_url.to_string(),
+            election_url: election_url.to_string(),
+        })
+    }
+
+    // #[flutter_rust_bridge::frb(ignore)]
+    pub async fn connect(&self) -> Result<PoolConnection<Sqlite>> {
+        Ok(self.pool.acquire().await?)
+    }
+}
 
 #[derive(Clone)]
 pub struct BFTContext {
