89a5c21fe0aada7d19bb7ea24681b5ddd455caa0
hhanh00 <hanh425@gmail.com>
2026-04-05 21:11:29 +0800

    fix: notes spent after the snapshot should be usable (#198)

--- tests/submit_election.sh
+++ tests/submit_election.sh
@@ -3,8 +3,7 @@
 yq eval -o json "$1.yml" > "$1.json"
 ELECTION_SEED="stool rich together paddle together pool raccoon promote attitude peasant latin concert"
 ./target/release/creator --election-file "$1.json" --seed "$ELECTION_SEED" --output-file "$1-pub.json"
-ELECTION=$(cat "$1-pub.json")
+ELECTION=$(jq -c . "$1-pub.json")
 ELECTION_REQ=$(jq -n --arg election "$ELECTION" '{"election": $election}')
-echo $ELECTION_REQ
 
 grpcurl -d "$ELECTION_REQ" --proto zcvlib/protos/vote.proto --plaintext localhost:9010 cash.z.vote.sdk.rpc.VoteStreamer/SetElection
